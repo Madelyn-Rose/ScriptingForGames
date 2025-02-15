37a244b28e358be2df9bfab394de1b5f57036a3d
Madelyn <madelyn.d.rose21@gmail.com>
2025-02-15 14:28:14 -0700

    Created animation controller
    still a work in progress

---
+++ AdventureGame/My project/Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        //Handles doube jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("JumpTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+
+        //Triggers Hit Animation
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("HitTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+
+        //Handles the Fall Animation
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("FallTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+    }
+}
--- AdventureGame/My project/Assets/Scripts/SimpleCharacterController.cs
+++ AdventureGame/My project/Assets/Scripts/SimpleCharacterController.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class SimpleCharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float moveSpeed = 5f;
+    public float jumpForce = 8f;
+    public float gravity = -9.81f;
     private CharacterController controller;
+    private Vector3 velocity;
     private Transform thisTransform;
-    private Vector3 movementVector = Vector3.zero;
 
     private void Start()
     {
@@ -20,13 +24,30 @@ public class SimpleCharacterController : MonoBehaviour
     private void Update()
     {
         MoveCharacter();
+        ApplyGravity();
         KeepCharacterOnXAxis();
     }
     private void MoveCharacter()
     {
-        movementVector.x = Input.GetAxis("Horizontal");
-        movementVector *= (moveSpeed * Time.deltaTime);
-        controller.Move(movementVector);
+        var moveInput = Input.GetAxis("Horizontal");
+        var move = new Vector3(moveInput, 0f, 0f) * (moveSpeed * Time.deltaTime);
+        controller.Move(move);
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+    }
+    private void ApplyGravity()
+    {
+        if (!controller.isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0f;
+        }
+        controller.Move(velocity * Time.deltaTime);
     }
     private void KeepCharacterOnXAxis()
     {
