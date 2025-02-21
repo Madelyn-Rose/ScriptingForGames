e88f4bbc1b23395a23d2632e2f64a171bb1ff29b
Madelyn <madelyn.d.rose21@gmail.com>
2025-02-20 18:45:17 -0700

    Created Enemy Triggers

---
+++ AdventureGame/My project/Assets/Scripts/SimpleTriggerEventBehavior.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleTriggerEventBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public UnityEvent triggerEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+        Debug.Log("Player interacted with the object!");
+    }
+}
